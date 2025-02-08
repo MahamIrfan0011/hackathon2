b669050c4cf400b0c8fba4994e06132c5fc52582
Maham Irfan_88 <160627752+MahamIrfan0011@users.noreply.github.com>
2025-02-08 10:19:54 +0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/images/**", // This ensures it matches the image paths under 'images' directory
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/studio/:path*",
+        destination: "/studio/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
